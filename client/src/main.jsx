a5edd7dd7aacf458fc82d528a7fee2a4437fb308
Shivakant <kants6397@gmail.com>
2024-04-04 14:00:34 +0530

    Add Redux store and Provider component

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,13 +2,17 @@ import './index.css'
 
 import ReactDOM from 'react-dom/client'
 import {Toaster} from 'react-hot-toast'
+import { Provider } from 'react-redux'
 import {BrowserRouter} from 'react-router-dom'
 
 import App from './App.jsx'
+import store from './redux/store.js'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <BrowserRouter>
+    <Provider store={store}>
     <App />
     <Toaster/>
+    </Provider>
   </BrowserRouter>,
 )
