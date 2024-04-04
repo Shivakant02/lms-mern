0d9206b29945b6d82fa7c9a43c3c7337359eaf6d
Shivakant <kants6397@gmail.com>
2024-04-04 19:13:23 +0530

    redux setup done

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -9,10 +9,11 @@ import App from './App.jsx'
 import store from './redux/store.js'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
-    <Provider store={store}>
-    <App />
-    <Toaster/>
-    </Provider>
-  </BrowserRouter>,
+ 
+  <Provider store={store}>
+    <BrowserRouter>
+      <App />
+      <Toaster/>
+    </BrowserRouter>
+  </Provider>
 )
