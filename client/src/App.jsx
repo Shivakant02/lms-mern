d815ae479268ba9189364ddb5504ceedae29a570
Shivakant <kants6397@gmail.com>
2024-04-04 13:38:08 +0530

    implemented react hot toast

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,11 @@
+import { useEffect } from "react"
+import toast from "react-hot-toast"
+
 export default function App() {
+  useEffect(
+    () => {
+    toast.success('hello')
+  })
   return (
     <h1 className="text-3xl font-bold underline">
       Hello world!
