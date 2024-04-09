67e3531bee4885b938e62e3cb51256d09752ca12
Shivakant <kants6397@gmail.com>
2024-04-09 23:11:57 +0530

    create course added successfully

--- server/controllers/course.controller.js
+++ server/controllers/course.controller.js
@@ -1,6 +1,8 @@
-
+import cludinary from 'cloudinary'
 import Course from "../models/course.model.js"
 import AppError from "../utils/appError.js"
+import fs from 'fs/promises'
+
 
 export const getAllCourses =async (req, res, next) => {
     try {
@@ -17,20 +19,79 @@ export const getAllCourses =async (req, res, next) => {
 
 export const getLecturesByCourseId = async(req, res, next) => {
     try {
-        const { courseId } = req.params;
-        const course = await Course.findById({ courseId });
-        
-        if (!course) {
-            return next(new AppError('Invalid course Id', 400));
-        }
+       const { id } = req.params;
 
-        res.status(200).json({
-            success: true,
-            message: 'Course lectures fetched successfully',
-            lecture:course.lectures
-            
-        })
+  const course = await Course.findById(id);
+
+  if (!course) {
+    return next(new AppError('Invalid course id or course not found.', 404));
+  }
+
+  res.status(200).json({
+    success: true,
+    message: 'Course lectures fetched successfully',
+    lectures: course.lectures,
+  });
+    
     } catch (error) {
         return next(new AppError(error.message, 500));
     }
+}
+
+
+export const createCourse = async(req, res, next) => {
+    try {
+        const { title, description, category, createdBy } = req.body;
+
+        if (!title || !description || !category || !createdBy) {
+            return next(new AppError('All fields are required',400));
+        }
+
+        const course = await Course.create({
+            title,
+            description,
+            category,
+            createdBy,
+            thumbnail: {
+                public_id: 'DUMMY',
+                secure_url:'DUMMY'
+            },
+        });
+
+        if (req.file) {
+
+            const result = await cludinary.v2.uploader.upload(req.file.path, {
+                folder: 'lms',
+            });
+
+
+            if (result) {
+                course.thumbnail.public_id = result.public_id;
+                course.thumbnail.secure_url = result.secure_url;
+            }
+
+            fs.rm(`uploads/${req.file.filename}`);
+        }
+
+        await course.save();
+
+        res.status(200).json({
+            success: true,
+            message: 'Course created successfully',
+            course
+        })
+         
+    } catch (error) {
+        return next(new AppError(error.message, 500));
+    }
+}
+
+
+export const deleteCourse = async(req, res, next) => {
+    
+}
+
+
+export const updateCourse = async(req, res, next) => {
+    
 }
