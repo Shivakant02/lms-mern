77551e6b54f717fe563bbd7a10271ba962801d71
Shivakant <kants6397@gmail.com>
2024-04-11 20:43:45 +0530

    payment routes and methods implemented

--- server/controllers/payment.controller.js
+++ server/controllers/payment.controller.js
@@ -1,4 +1,5 @@
 import User from "../models/user.model.js";
+import crypto from "crypto";
 import { razorpay } from "../server.js";
 import AppError from "../utils/appError.js";
 import Payment from "../models/payment.model.js";
@@ -25,12 +26,13 @@ export const buySubscription = async (req, res, next) => {
     }
 
     if (user.role === "ADMIN") {
-      return next(new AppError("Asmin can not purchase a subscription!", 400));
+      return next(new AppError("Admin can not purchase a subscription!", 400));
     }
 
     const subscription = await razorpay.subscriptions.create({
       plan_id: process.env.RAZORPAY_PLAN_ID,
       customer_notify: 1,
+      total_count: 12,
     });
 
     user.subscription.id = subscription.id;
@@ -41,6 +43,7 @@ export const buySubscription = async (req, res, next) => {
     res.status(200).json({
       success: true,
       message: "Subscribed successfully",
+      subscription_id: subscription.id,
     });
   } catch (error) {
     return next(new AppError(error.message, 500));
@@ -50,37 +53,43 @@ export const buySubscription = async (req, res, next) => {
 export const verifySubscription = async (req, res, next) => {
   try {
     const { id } = req.user;
-    const user = await User.findById(id);
-
-    if (!user) {
-      return next(new AppError("Unauthorized, please login", 400));
-    }
-
     const {
       razorpay_payment_id,
-      razorpay_signature,
       razorpay_subscription_id,
+      razorpay_signature,
     } = req.body;
 
+    // Finding the user
+    const user = await User.findById(id);
+
+    // Getting the subscription ID from the user object
+    const subscriptionId = user.subscription.id;
+
+    // Generating a signature with SHA256 for verification purposes
+    // Here the subscriptionId should be the one which we saved in the DB
+    // razorpay_payment_id is from the frontend and there should be a '|' character between this and subscriptionId
+    // At the end convert it to Hex value
     const generatedSignature = crypto
       .createHmac("sha256", process.env.RAZORPAY_SECRET_KEY)
-      .update(`${razorpay_payment_id}|${razorpay_subscription_id}`);
+      .update(`${razorpay_payment_id}|${subscriptionId}`)
+      .digest("hex");
 
+    // Check if generated signature and signature received from the frontend is the same or not
     if (generatedSignature !== razorpay_signature) {
-      return next(new AppError("Payment not verified, please try again", 400));
+      return next(new AppError("Payment not verified, please try again.", 400));
     }
 
-    //record payment details in payment collection
-
+    // If they match create payment and store it in the DB
     await Payment.create({
       razorpay_payment_id,
-      razorpay_signature,
       razorpay_subscription_id,
+      razorpay_signature,
     });
 
-    //update user records with subscription status
-
+    // Update the user subscription status to active (This will be created before this)
     user.subscription.status = "active";
+
+    // Save the user in the DB with any changes
     await user.save();
 
     res.status(200).json({
@@ -124,6 +133,17 @@ export const cancelSubscription = async (req, res, next) => {
 
 export const getAllPayments = async (req, res, next) => {
   try {
+    const { count } = req.query;
+
+    const subscription = await razorpay.subscriptions.all({
+      count: count || 10,
+    });
+
+    res.status(200).json({
+      success: true,
+      message: "All payments",
+      payments: subscription,
+    });
   } catch (error) {
     return next(new AppError(error.message, 500));
   }
--- server/models/user.model.js
+++ server/models/user.model.js
@@ -1,106 +1,101 @@
 import { Schema, model } from "mongoose";
-import bcrypt from 'bcryptjs'
-import jwt from 'jsonwebtoken'
-import crypto from 'crypto'
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+import crypto from "crypto";
 
-const userSchema = new Schema({
+const userSchema = new Schema(
+  {
     fullName: {
-        type: String,
-        required: [true, 'Full name is required'],
-        trim: true,
-        minLength: [4, 'Full name must be at least 4 characters'],
-        maxLength: [50, 'Full name must be at most 50 characters'],
-        
+      type: String,
+      required: [true, "Full name is required"],
+      trim: true,
+      minLength: [4, "Full name must be at least 4 characters"],
+      maxLength: [50, "Full name must be at most 50 characters"],
     },
-    email:
-    {
-        type: String,
-        required: [true, 'Email is required'],
-        unique: true,
-        lowercase: true,
-        trim: true,
-        match: [
+    email: {
+      type: String,
+      required: [true, "Email is required"],
+      unique: true,
+      lowercase: true,
+      trim: true,
+      match: [
         /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
-        'Please fill in a valid email address',
+        "Please fill in a valid email address",
       ],
-
     },
     password: {
-        type: String,
-        required: [true, 'Password is required'],
-        minLength:[8,'Password must be atleast 8 characters'],
-        select: false,
+      type: String,
+      required: [true, "Password is required"],
+      minLength: [8, "Password must be atleast 8 characters"],
+      select: false,
     },
     role: {
-        type: String,
-        enum: ['USER', 'ADMIN'],
-        default:'USER'
+      type: String,
+      enum: ["USER", "ADMIN"],
+      default: "USER",
     },
     avatar: {
-        Public_id: {
-            type: String,
-            // required: true,
-        },
-        secure_url: {
-            type: String,
-            // required: true,
-        },
-
+      Public_id: {
+        type: String,
+        // required: true,
+      },
+      secure_url: {
+        type: String,
+        // required: true,
+      },
     },
     forgotPasswordToken: String,
     forgotPasswordExpiry: Date,
     subscription: {
-        id: String,
-        status: String,
-        
-    }
+      id: String,
+      status: String,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
 
-}, {
-    timestamps: true
-});
-
-userSchema.pre('save', async function (next) {
-    if (!this.isModified('password')) {
-        return next();
-    }
-    this.password = await bcrypt.hash(this.password, 10);
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) {
+    return next();
+  }
+  this.password = await bcrypt.hash(this.password, 10);
 });
 
 userSchema.methods = {
-    comparePassword: async function (plainTextPassword) {
-        return await bcrypt.compare(plainTextPassword,this.password);
-    },
+  comparePassword: async function (plainTextPassword) {
+    return await bcrypt.compare(plainTextPassword, this.password);
+  },
 
-    generateJWTToken: async function () {
-        return await jwt.sign(
-            {
-                id: this._id,
-                role: this.role,
-                email: this.email,
-                subscription:this.subscription
-            },
-            process.env.JWT_SECRET,
-            {
-                expiresIn: process.env.JWT_EXPIRES_IN
-            }
-        )
-    },
+  generateJWTToken: async function () {
+    return await jwt.sign(
+      {
+        id: this._id,
+        role: this.role,
+        email: this.email,
+        subscription: this.subscription,
+      },
+      process.env.JWT_SECRET,
+      {
+        expiresIn: process.env.JWT_EXPIRES_IN,
+      }
+    );
+  },
 
-    generatePasswordToken: async function () {
-        const resetToken = crypto.randomBytes(20).toString('hex');
+  generatePasswordToken: async function () {
+    const resetToken = crypto.randomBytes(20).toString("hex");
 
-        this.forgotPasswordToken = crypto
-            .createHash('sha256')
-            .update(resetToken)
-            .digest('hex')
-        ;
-        
-        this.forgotPasswordExpiry = Date.now() + 15 * 60 * 1000;
+    this.forgotPasswordToken = crypto
+      .createHash("sha256")
+      .update(resetToken)
+      .digest("hex");
 
-        return resetToken;
-    }
+    this.forgotPasswordExpiry = Date.now() + 15 * 60 * 1000;
 
-}
+    return resetToken;
+  },
+};
 
-const User = model('user', userSchema);
+const User = model("user", userSchema);
 export default User;
