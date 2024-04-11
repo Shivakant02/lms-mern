4439d752b3aea68e21cfe5ded8dca875325f2b76
Shivakant <kants6397@gmail.com>
2024-04-11 18:33:48 +0530

    razorpay added

--- server/routes/course.routes.js
+++ server/routes/course.routes.js
@@ -1,36 +1,40 @@
 import { Router } from "express";
-import { addLectureToCourseById, createCourse, deleteCourse, getAllCourses, getLecturesByCourseId, updateCourse } from "../controllers/course.controller.js";
-import { authorizedRoles, isLoggedIn } from "../middleware/auth.middleware.js";
+import {
+  addLectureToCourseById,
+  createCourse,
+  deleteCourse,
+  getAllCourses,
+  getLecturesByCourseId,
+  updateCourse,
+} from "../controllers/course.controller.js";
+import {
+  authorizedRoles,
+  authorizedSubscriber,
+  isLoggedIn,
+} from "../middleware/auth.middleware.js";
 import upload from "../middleware/multer.middleware.js";
-
 
 const router = Router();
 
 router
-    .route('/')
-    .get(getAllCourses)
-    .post(
-        isLoggedIn,
-        authorizedRoles('ADMIN'),
-        upload.single('thumbnail'),
-        createCourse
-    );
+  .route("/")
+  .get(getAllCourses)
+  .post(
+    isLoggedIn,
+    authorizedRoles("ADMIN"),
+    upload.single("thumbnail"),
+    createCourse
+  );
 router
-    .route('/:id')
-    .get(isLoggedIn, getLecturesByCourseId)
-    .put(
-        isLoggedIn,
-        authorizedRoles('ADMIN'),
-        updateCourse)
-    .delete(isLoggedIn,
-        authorizedRoles('ADMIN'),
-        deleteCourse)
-    .post(
-        isLoggedIn,
-        authorizedRoles('ADMIN'),
-        upload.single('lecture'),
-        addLectureToCourseById)
-
-    
+  .route("/:id")
+  .get(isLoggedIn, authorizedSubscriber, getLecturesByCourseId)
+  .put(isLoggedIn, authorizedRoles("ADMIN"), updateCourse)
+  .delete(isLoggedIn, authorizedRoles("ADMIN"), deleteCourse)
+  .post(
+    isLoggedIn,
+    authorizedRoles("ADMIN"),
+    upload.single("lecture"),
+    addLectureToCourseById
+  );
 
 export default router;
