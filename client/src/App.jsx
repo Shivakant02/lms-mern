9453bd9d5f778ba2c51e54d8ed2ef627c723d47c
Shivakant <kants6397@gmail.com>
2024-04-16 04:46:35 +0530

    page Not found added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,12 +2,14 @@ import { Route, Routes } from "react-router-dom";
 
 import Aboutus from "./pages/Aboutus";
 import Home from "./pages/Home";
+import NotFound from "./pages/NotFound";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/about" element={<Aboutus />} />
+      <Route path="*" element={<NotFound />} />
     </Routes>
   );
 }
