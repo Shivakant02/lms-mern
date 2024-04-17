1855665f006d0c0f805aeda929ded6b3d83ec5e4
Shivakant <kants6397@gmail.com>
2024-04-17 22:20:13 +0530

    added course slice to redux devtools

---
+++ client/src/redux/slices/courseSlice.js
@@ -0,0 +1,35 @@
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import toast from "react-hot-toast";
+
+import axiosInstance from "../../config/axiosInstance";
+const initialState = {
+  courseList: [],
+};
+
+export const getAllCourses = createAsyncThunk(
+  "/course/getAllCourses",
+  async (data) => {
+    try {
+      const response = axiosInstance.get("/courses", data);
+      toast.promise(response, {
+        loading: "Wait! fetching all your courses",
+        success: (data) => {
+          return data?.data?.message;
+        },
+        error: "Failed to load courses",
+      });
+      return await response;
+    } catch (error) {
+      //console.log(error);
+      toast.error(error?.response?.data?.message);
+    }
+  }
+);
+
+const courseSlice = createSlice({
+  name: "course",
+  initialState,
+  reducers: {},
+});
+
+export default courseSlice.reducer;
