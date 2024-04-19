68a3538bf8a76ed6542e9bf402a3c81f87844ca4
Shivakant <kants6397@gmail.com>
2024-04-19 17:18:23 +0530

    create course async thunk created

--- client/src/redux/slices/courseSlice.js
+++ client/src/redux/slices/courseSlice.js
@@ -26,13 +26,37 @@ export const getAllCourses = createAsyncThunk(
   }
 );
 
+export const createCourse = createAsyncThunk("/course/create", async (data) => {
+  try {
+    let formData = new FormData();
+    formData.append("title", data?.title);
+    formData.append("description", data?.description);
+    formData.append("category", data?.category);
+    formData.append("createdby", data?.createdBy);
+    formData.append("thumbnail", data?.thumbnail);
+
+    const response = axiosInstance.post("/courses", formData);
+    toast.promise(response, {
+      loading: "Wait! creating new course",
+      success: (data) => {
+        return data?.data?.message;
+      },
+      error: "Failed to create  course",
+    });
+    return (await response).data;
+  } catch (error) {
+    //console.log(error);
+    toast.error(error?.response?.data?.message);
+  }
+});
+
 const courseSlice = createSlice({
   name: "course",
   initialState,
   reducers: {},
   extraReducers: (builder) => {
     builder.addCase(getAllCourses.fulfilled, (state, action) => {
-      console.log(action.payload);
+      // console.log(action.payload);
       if (action?.payload) {
         state.courseList = [...action.payload];
       }
