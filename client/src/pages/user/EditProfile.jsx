28774e050c5dd91c02fa951c935afb481480e028
Shivakant <kants6397@gmail.com>
2024-04-30 15:11:30 +0530

    Refactor EditProfile page to include image upload and update profile functionality

--- client/src/pages/user/EditProfile.jsx
+++ client/src/pages/user/EditProfile.jsx
@@ -1,7 +1,9 @@
 import { useState } from "react";
 import toast from "react-hot-toast";
+import { BsPersonCircle } from "react-icons/bs";
 import { useDispatch, useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 import HomeLayout from "../../layouts/HomeLayout";
 import { getUserData, updateProfile } from "../../redux/slices/authSlice";
@@ -66,7 +68,60 @@ function EditProfile() {
 
   return (
     <HomeLayout>
-      <div>Edit profile</div>
+      <div className=" flex items-center justify-center h-[90vh]">
+        <form
+          onSubmit={onFormSubmit}
+          className=" flex flex-col justify-center gap-5 rounded-lg p-4 text-white w-80 min-h-[26rem] border"
+        >
+          <h1 className=" text-center text-2xl font-semibold ">Edit profile</h1>
+          <label htmlFor="image_upload" className=" cursor-pointer">
+            {data.previewImage ? (
+              <img
+                src={data.previewImage}
+                className=" w-28 h-28 rounded-full m-auto "
+              />
+            ) : (
+              <BsPersonCircle className=" w-28 h-28 rounded-full m-auto" />
+            )}
+          </label>
+          <input
+            onChange={handleImageUpload}
+            type="file"
+            name="image_upload"
+            id="image_upload"
+            accept=".jpg,.png,.jpeg,.svg"
+            className=" hidden"
+          />
+          <div className=" flex flex-col gap-1">
+            <label htmlFor="fullName" className=" text-lg font-semibold  ">
+              Fullname
+            </label>
+            <input
+              required
+              name="fullName"
+              id="fullName"
+              placeholder="Enter your name..."
+              className=" bg-transparent px-2 py-1 border outline-none rounded-md"
+              type="text"
+              value={data.fullName}
+              onChange={handleInputChange}
+            />
+          </div>
+          <button
+            className=" w-full btn btn-sm btn-outline   btn-secondary"
+            type=" submit"
+          >
+            Update profile
+          </button>
+          <Link
+            className=" link text-accent cursor-pointer flex items-center justify-center "
+            to="/user/profile"
+          >
+            {" "}
+            <p>Go back to profile</p>
+          </Link>
+        </form>
+      </div>
     </HomeLayout>
   );
 }
