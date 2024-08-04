d90e0614361c45a4a2c4b5a964fd6d6875177b96
Shivakant <kants6397@gmail.com>
2024-08-04 13:49:10 +0530

    space fix

--- server/middleware/error.middleware.js
+++ server/middleware/error.middleware.js
@@ -1,13 +1,12 @@
 const errorMiddleware = (error, req, res, next) => {
-    
-    req.statusCode = req.statusCode || 500
-    req.message = req.message || 'Something went wrong!'
+  req.statusCode = req.statusCode || 500;
+  req.message = req.message || "Something went wrong!";
 
-    res.status(req.statusCode).json({
-        success: false,
-        message: req.message,
-        stack: error.stack
-    });
-}
+  res.status(req.statusCode).json({
+    success: false,
+    message: req.message,
+    stack: error.stack,
+  });
+};
 
 export default errorMiddleware;
